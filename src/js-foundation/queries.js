441c7e4846cdc8af1bb6b57e5b1a492dbb3372db
Tuturu <edu.sanchez@unmsm.edu.pe>
2024-11-26 20:35:21 -0500

    raro

--- src/js-foundation/queries.js
+++ src/js-foundation/queries.js
@@ -1,19 +1,35 @@
+const { connect } = require('../plugins/get-conexion.plugin'); // Importa el método para obtener la conexión
 
-function verifyAccess(conexion, dni, correo_electronico) {
+async function verifyAccess(dni, correo_electronico) {
     return new Promise((resolve, reject) => {
-        const query = 'CALL verificar_acceso(?, ?)';
-        
-        conexion.query(query, [dni, correo_electronico], (error, results) => {
+        const connection = connect();  // Obtiene la conexión
+
+        // Verificar si la conexión está abierta antes de ejecutar la consulta
+        if (connection.state === 'disconnected') {
+            console.log("La conexión está cerrada. Intentando reconectar...");
+            // Intentamos reconectar
+            connection.connect(err => {
+                if (err) {
+                    console.error('Error al reconectar con la base de datos:', err);
+                    return reject(err);  // Si no puede reconectar, rechazamos la promesa
+                }
+                console.log("Reconectado a la base de datos.");
+            });
+        }
+
+        const query = 'CALL verificar_acceso(?, ?)';  // Procedimiento almacenado
+
+        // Ejecutar la consulta en la conexión abierta
+        connection.query(query, [dni, correo_electronico], (error, results) => {
             if (error) {
-                return reject(error);
+                console.error('Error en la consulta:', error);
+                return reject(error);  // Si ocurre un error, rechazamos la promesa
             }
-            
-            
+
             const resultado = results[0] && results[0][0] && results[0][0].resultado;
             const estudiante_id = results[0] && results[0][0] && results[0][0].estudiante_id;
 
-            
-            // Verificar si `resultado` es 1 y obtener `id_estudiante`
+            // Verificar si `resultado` es 1 y retornar el `id_estudiante`
             if (resultado === 1) {
                 resolve({
                     acceso_permitido: true,
