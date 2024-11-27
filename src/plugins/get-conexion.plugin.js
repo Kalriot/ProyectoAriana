b014c9d7442c09ff831bba4ad42f84686a5bcf25
Tuturu <edu.sanchez@unmsm.edu.pe>
2024-11-27 00:12:21 -0500

    vuelve comoantes

--- src/plugins/get-conexion.plugin.js
+++ src/plugins/get-conexion.plugin.js
@@ -1,43 +1,39 @@
-
-
 const mysql = require('mysql2');
-const { endConnection } = require('.');
 
-// Crear el pool de conexiones
-const pool = mysql.createPool({
-    host: 'blsj1wqmfmkypexcxtvj-mysql.services.clever-cloud.com',
-    user: 'uyean69divikr5u4',
-    password: 'OS3aXufqHwFbTrhzTrPj',
-    database: 'blsj1wqmfmkypexcxtvj',
-    waitForConnections: true, // Esperar conexiones si el pool está lleno
-    connectionLimit: 10,      // Número máximo de conexiones en el pool
-    queueLimit: 0             // Sin límite en la cola de espera
-});
 
-// Función para obtener una conexión del pool
-function getConnection() {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, connection) => {
-            if (err) {
-                return reject(err); // Si hay un error, rechazar la promesa
-            }
-            resolve(connection); // Si la conexión se obtiene correctamente, resolver la promesa
+let connection;
+
+function connect() {
+    if (!connection) {
+        connection = mysql.createConnection({
+            host: 'blsj1wqmfmkypexcxtvj-mysql.services.clever-cloud.com',
+            user: 'uyean69divikr5u4',
+            password: 'OS3aXufqHwFbTrhzTrPj',
+            database: 'blsj1wqmfmkypexcxtvj',
         });
-    });
+        connection.connect(err => {
+            if (err) {
+                console.error('Error  al conectar con la base de datos:', err);
+            } else {
+                console.log('Conectado a la base de datos');
+            }
+        });
+    }
+    return connection;
 }
-
-// Función para cerrar todas las conexiones del pool
-function endPool() {
-    pool.end(err => {
-        if (err) {
-            console.error('Error al cerrar el pool de conexiones:', err);
-        } else {
-            console.log('Pool de conexiones cerrado');
-        }
-    });
+function endConnection() {
+    if (connection) {
+        connection.end(err => {
+            if (err) {
+                console.error('Error al cerrar la conexión:', err);
+            } else {
+                console.log('Conexión cerrada');
+            }
+        });
+    }
 }
 
 module.exports = {
-    getConnection,
+    connect,
     endConnection
 };
